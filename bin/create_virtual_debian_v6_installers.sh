dfc7af1299f26baf48191895d408517cc644fc48
James Palmer <jpalmer@pivotal.io>
2020-06-29 14:09:48 -0700

    WIP

--- bin/create_virtual_debian_v6_installers.sh
+++ bin/create_virtual_debian_v6_installers.sh
@@ -16,19 +16,33 @@ create_installer() {
   version="$3"
 
   mkdir -p "$template_dir/DEBIAN"
+  mkdir -p "$template_dir/usr/bin"
   template="$template_dir/DEBIAN/control"
   log "$version: Creating $arch installer at $template..."
 
   # SIZE="$(BLOCKSIZE=1000 du $root/extracted-binaries/cf-cli_linux_x86-64 | cut -f 1)"
   cp ${CF6_TEMPLATE} $template
-  # echo "Installed-Size: ${SIZE}" >> $template 
+  # echo "Installed-Size: ${SIZE}" >> $template
   echo "Version: $version" >> $template
-  echo "Architecture: amd64" >> $template
-  echo "Depends: cf-cli (= $version)" >> $template
+  if test "$arch" = "x86-64"; then
+    echo "I'm 64bit!"
+    echo $version $arch
+    echo "Architecture: amd64" >> $template
+  fi
+  if test "$arch" = "i686"; then
+    echo "I'm 32bit!"
+    echo $version $arch
+    echo "Architecture: i386" >> $template
+  fi
+  # echo "Depends: cf-cli (= $version)" >> $template
 
+  tar -xf $dir/cf-cli_${version}_linux_${arch}.tgz cf
+  mv cf $template_dir/usr/bin
 
-  fakeroot dpkg --build $template_dir $dir/cf-cli-installer_x86-64.deb
-  # TODO: sign_installer $dir/$arch/*
+  tree $template_dir
+
+  fakeroot dpkg --build $template_dir $dir/cf6-cli-installer_${version}_${arch}.deb
+  # fakeroot dpkg --build $template_dir $dir/cf6-cli-installer_x86-64.deb
 }
 
 create_installers_for_version_dir() {
@@ -45,8 +59,8 @@ create_installers_for_version_dir() {
     break
   done
 
-  # create_installer "i386" "$installer_template" "$version"
-  create_installer "x86_64" "$installer_template" "$version"
+  create_installer "i686" "$installer_template" "$version"
+  create_installer "x86-64" "$installer_template" "$version"
 }
 
 sign_installer() {
@@ -93,24 +107,27 @@ main() {
   # > cd /tmp/cli-ci
   # > aws configure # enter the Administrator credentials
   # > bin/create_virtual_debian_v6_installers.sh "s3://cf-cli-releases/releases/" "/tmp/certificates/Cloudfoundry-Private-PGP.gpg"
-  # > aws s3 sync $release_dir s3://cf-cli-releases/releases # should upload all cf6 rpms
-  # > ?????createrepo -s sha $release_dir # creates a repodata folder
-  # > aws sync $release_dir/repodata s3://cf-cli-rpm-repo/repodata
+  # > aws s3 sync $release_dir s3://v6-cf-cli-releases/releases # should upload all cf6 rpms
 
   s3_bucket_path=$1
   gpg_key_location=$2
-  # release_dir="$(mktemp -d)"
-  release_dir="/tmp/tmp.0Moqt0vJkI"
+  # release_dir="$DIR/temp-s3-releases"
+  release_dir="$DIR/subset-releases"
+  # release_dir="/tmp/tmp.0Moqt0vJkI/"
   templates_dir="$(mktemp -d)"
   gpg_dir="$(mktemp -d)"
 
+  mkdir -p "$release_dir"
   trap "rm -rf $gpg_dir" 0
 
   log "Release path: $release_dir"
   log "Template path: $templates_dir"
   log "GPG path: $gpg_dir"
 
-  # aws s3 sync $s3_bucket_path $release_dir
+  echo $s3_bucket_path
+  echo $release_dir
+
+  aws s3 sync $s3_bucket_path $release_dir
 
 
   cd $release_dir
