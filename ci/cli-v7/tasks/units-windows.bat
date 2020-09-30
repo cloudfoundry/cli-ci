ec1d3ec9b816d099b7261411114fe8a3efff83a4
James Palmer <jpalmer@pivotal.io>
2020-09-30 22:22:51 +0000

    Use correct RefreshEnv path
    [#174891518](https://www.pivotaltracker.com/story/show/174891518)
    
    Co-authored-by: Sebastian Vidrio <svidrio@vmware.com>

--- ci/cli-v7/tasks/units-windows.bat
+++ ci/cli-v7/tasks/units-windows.bat
@@ -1,6 +1,7 @@
 SET TARGET_V7=true
 SET GOPATH=%CD%\gopath
-call RefreshEnv.cmd
+call C:\ProgramData\chocolatey\bin\RefreshEnv.cmd
+
 SET PATH=C:\Go\bin;C:\Program Files\Git\cmd\;%GOPATH%\bin;%PATH%
 
 cd %GOPATH%\src\code.cloudfoundry.org\cli
