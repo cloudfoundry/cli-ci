199273f8fbbbb1f23149a9e2827d8baf03bbadff
Ryker Reed <ryker.reed3@gmail.com>
2021-03-25 14:40:44 -0700

    debugging go errors

--- ci/cli-v7/tasks/units-windows.ps1
+++ ci/cli-v7/tasks/units-windows.ps1
@@ -3,12 +3,21 @@ Import-Module C:\ProgramData\chocolatey\helpers\chocolateyProfile.psm1
 refreshenv
 cd $Env:ROOT
 
+echo "Working Directory: $pwd"
+echo "GOPATH:            $Env:GOPATH"
+
 $Env:GOPATH="$pwd\go"
 $Env:PATH="$Env:GOPATH\bin;" + "$Env:PATH"
 $Env:PATH="$pwd;" + "$Env:PATH"
+
+echo "(2nd time) Working Directory: $pwd"
+echo "(2nd time) GOPATH:            $Env:GOPATH"
+
 pushd $Env:GOPATH\src\code.cloudfoundry.org\cli
 	set-executionpolicy remotesigned
-
+	
+	echo "debugging"
+	
 	go version
 
 	go get -u github.com/onsi/ginkgo/ginkgo
