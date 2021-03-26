06d599a2b24d9f9143337550b32d86593fb026ec
Alexander Berezovsky <aberezovsky@vmware.com>
2021-03-26 11:19:19 +0000

    Fix windows units run script

--- ci/cli-v7/tasks/units-windows.ps1
+++ ci/cli-v7/tasks/units-windows.ps1
@@ -1,21 +1,28 @@
 $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-echo "PWD: $pwd"
+echo "Work Directory: $pwd"
 
 $Env:ROOT="$pwd"
 Import-Module C:\ProgramData\chocolatey\helpers\chocolateyProfile.psm1
 refreshenv
 cd $Env:ROOT
 
-go get -v -u github.com/onsi/ginkgo/ginkgo
+$null = New-Item -ItemType Directory -Force -Path $Env:TEMP
+
+$Env:GOPATH="$Env:ROOT\go"
+$Env:PATH="$Env:GOPATH\bin;" + "$Env:PATH"
+
+if ((Get-Command "ginkgo.exe" -ErrorAction SilentlyContinue) -eq $null) {
+  go get -v -u github.com/onsi/ginkgo/ginkgo
+}
 
 pushd $Env:GOPATH\src\code.cloudfoundry.org\cli
-	set-executionpolicy remotesigned
-	
-	Get-ChildItem Env: | Format-Table -Wrap -AutoSize
-	go version
-	
-	$Env:GOFLAGS="-mod=vendor"
-	ginkgo -r -randomizeAllSpecs -randomizeSuites -skipPackage integration -flakeAttempts=2 -tags="V7" .
+  set-executionpolicy remotesigned
+
+  go version
+  ginkgo version
+
+  $Env:GOFLAGS="-mod=vendor"
+  ginkgo -r -randomizeAllSpecs -randomizeSuites -skipPackage integration -flakeAttempts=2 -tags="V7" .
 popd
