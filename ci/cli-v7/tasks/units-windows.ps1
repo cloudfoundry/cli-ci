d8918a464d6be971aecbcc285f652161fb926e7a
Alexander Berezovsky <aberezovsky@vmware.com>
2021-03-26 09:48:20 +0000

    Revert debugging

--- ci/cli-v7/tasks/units-windows.ps1
+++ ci/cli-v7/tasks/units-windows.ps1
@@ -3,32 +3,12 @@ Import-Module C:\ProgramData\chocolatey\helpers\chocolateyProfile.psm1
 refreshenv
 cd $Env:ROOT
 
-echo "Working Directory: $pwd"
-echo "GOPATH:            $Env:GOPATH"
-
-$Env:GOPATH="$pwd\go"
 $Env:PATH="$Env:GOPATH\bin;" + "$Env:PATH"
 $Env:PATH="$pwd;" + "$Env:PATH"
-
-echo "(2nd time) Working Directory: $pwd"
-echo "(2nd time) GOPATH:            $Env:GOPATH"
-echo "PATH:                         $Env:PATH"
-
 pushd $Env:GOPATH\src\code.cloudfoundry.org\cli
 	set-executionpolicy remotesigned
-	
-	echo "debugging 1"
-	
-	echo "Working Directory: $pwd"
-	echo "GOPATH:            $Env:GOPATH"
-	
-	echo "debugging 2"
-	
-	$Env:GOPATH\bin\go version
 
-	$Env:GOPATH\bin\go get -u github.com/onsi/ginkgo/ginkgo
-
-	ginkgo version
+  go version
 
 	$Env:GOFLAGS="-mod=vendor"
 	ginkgo -r -randomizeAllSpecs -randomizeSuites -skipPackage integration -flakeAttempts=2 -tags="V7" .
