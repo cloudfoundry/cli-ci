84cd9f9a818b3ed9581a1f592aa6129d65c061c5
Al Berez <a-b@users.noreply.github.com>
2021-03-26 02:57:49 -0700

    Debug

--- ci/cli-v7/tasks/units-windows.ps1
+++ ci/cli-v7/tasks/units-windows.ps1
@@ -1,15 +1,24 @@
+echo "PWD: $pwd"
+Get-ChildItem Env: | Format-Table -Wrap -AutoSize
+
 $Env:ROOT="$pwd"
 Import-Module C:\ProgramData\chocolatey\helpers\chocolateyProfile.psm1
 refreshenv
 cd $Env:ROOT
 
+Get-ChildItem Env: | Format-Table -Wrap -AutoSize
+
 $Env:PATH="$Env:GOPATH\bin;" + "$Env:PATH"
 $Env:PATH="$pwd;" + "$Env:PATH"
+
+Get-ChildItem Env: | Format-Table -Wrap -AutoSize
+
 pushd $Env:GOPATH\src\code.cloudfoundry.org\cli
 	set-executionpolicy remotesigned
-
-  go version
-
+	
+	Get-ChildItem Env: | Format-Table -Wrap -AutoSize
+	go version
+	
 	$Env:GOFLAGS="-mod=vendor"
 	ginkgo -r -randomizeAllSpecs -randomizeSuites -skipPackage integration -flakeAttempts=2 -tags="V7" .
 popd
