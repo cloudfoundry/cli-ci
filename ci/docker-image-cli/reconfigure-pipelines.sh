2b4942990b8f05dae9bd8a76fb1b6ed6596ae510
Juan Diego Gonzalez <gojuan@vmware.com>
2021-07-12 21:56:48 +0000

    Configure pipelines for building and publishing cli images
    Finished configuring the pipelines for publishing cli docker images.
    Decided to have a script which will be in charge of flying the pipelines
    as new major versions are added.
    
    The pipeline ingests the branch associated with the major version and
    expects the dockerfile to be located under the `docker`
    folder in the cli repo.
    
    Co-authored-by: Dominic Roberts <dominicr@vmware.com>

--- ci/docker-image-cli/reconfigure-pipelines.sh
+++ ci/docker-image-cli/reconfigure-pipelines.sh
@@ -2,6 +2,8 @@
 
 set -e
 set -o pipefail
+cli_version=$1
+cli_branch=$2
 
 check_installed() {
   if ! command -v $1 > /dev/null 2>&1; then
@@ -13,7 +15,6 @@ check_installed() {
 configure_pipeline() {
   local name=$1
   local pipeline=$2
-
   printf "configuring the $name pipeline...\n"
 
   fly -t ci set-pipeline \
@@ -21,7 +22,9 @@ configure_pipeline() {
     -c $pipeline \
     -l <(lpass show 'Shared-CLI/Release Process/release-pipeline-concourse-credentials-dev.yml' --notes) \
     --var="username=$(lpass show 'Shared-CLI/CF CLI Dockerhub' --username)" \
-    --var="password=$(lpass show 'Shared-CLI/CF CLI Dockerhub' --password)"
+    --var="password=$(lpass show 'Shared-CLI/CF CLI Dockerhub' --password)" \
+    --var="cli-version=$cli_version" \
+    --var="cli-branch=$cli_branch"
 }
 check_installed lpass
 check_installed fly
@@ -30,4 +33,4 @@ check_installed fly
 lpass sync
 
 
-configure_pipeline cli-docker-image pipeline.yml
+configure_pipeline cf-cli-v$1-docker-image pipeline.yml
