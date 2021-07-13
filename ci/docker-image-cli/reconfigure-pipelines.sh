3186f5ea334cce727207f7c81ee5aa75ab3bdaf5
Juan Diego González <25335192+jdgonzaleza@users.noreply.github.com>
2021-07-13 12:31:24 -0500

    Cf cli docker image pipeline [#178760135](https://www.pivotaltracker.com/story/show/178760135)
    * Moved test docker task to shared and configured it to validate image by
    version
    
    * Using .sh in test task as alpine doesn't have bash
    
    * Configure pipelines for building and publishing cli images
    
    * Renamed cf-cli docker image
    
    * Renamed the cf-cli-image in the build plan. 
    
    Co-authored-by: Ryker Reed <rykerr@vmware.com>
    Co-authored-by: Dominic Roberts <dominicr@vmware.com>

---
+++ ci/docker-image-cli/reconfigure-pipelines.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+cli_version=$1
+cli_branch=$2
+
+check_installed() {
+  if ! command -v $1 > /dev/null 2>&1; then
+    printf "$1 must be installed before running this script!"
+    exit 1
+  fi
+}
+
+configure_pipeline() {
+  local name=$1
+  local pipeline=$2
+  printf "configuring the $name pipeline...\n"
+
+  fly -t ci set-pipeline \
+    -p $name \
+    -c $pipeline \
+    -l <(lpass show 'Shared-CLI/Release Process/release-pipeline-concourse-credentials-dev.yml' --notes) \
+    --var="username=$(lpass show 'Shared-CLI/CF CLI Dockerhub' --username)" \
+    --var="password=$(lpass show 'Shared-CLI/CF CLI Dockerhub' --password)" \
+    --var="cli-version=$cli_version" \
+    --var="cli-branch=$cli_branch"
+}
+check_installed lpass
+check_installed fly
+
+# Make sure we're up to date and that we're logged in.
+lpass sync
+
+
+configure_pipeline cf-cli-v$1-docker-image pipeline.yml
