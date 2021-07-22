b99abb8c0847de7eb83d7033ca593d466e2c5e8c
Juan Diego Gonzalez <gojuan@vmware.com>
2021-07-22 21:59:55 +0000

    Fix windows worker configuring pipeline
    Co-authored-by: Alexander Berezovsky <aberezovsky@vmware.com>

--- ci/shared/tasks/windows-setup.ps1
+++ ci/shared/tasks/windows-setup.ps1
@@ -18,7 +18,7 @@ refreshenv
 cd $Env:ROOT
 
 if ((Get-Command "go" -ErrorAction SilentlyContinue) -eq $null) {
-  choco install --no-progress -r -y golang --force
+  choco install --no-progress -r -y golang --version 1.16.6 --force
 }
 
 if ((Get-Command "git" -ErrorAction SilentlyContinue) -eq $null) {
