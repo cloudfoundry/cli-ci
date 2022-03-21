25ceacb8ab33cdfaba8188e762d69da0a0ec7c58
Shwetha Gururaj <gururajsh@vmware.com>
2022-03-21 11:13:55 -0400

    WIP:Changes vars

--- ci/shared/tasks/integration-windows-setup-toolsmiths.ps1
+++ ci/shared/tasks/integration-windows-setup-toolsmiths.ps1
@@ -3,15 +3,15 @@ trap { $host.SetShouldExit(1) }
 
 . "$PSScriptRoot\windows-setup.ps1"
 
-# $DOMAIN=(Get-Content $pwd\bosh-lock\name -Raw).trim()
+$CF_INT_NAME=(Get-Content $pwd\cf-credentials\env-name -Raw).trim()
 $Env:CF_INT_PASSWORD=(Get-Content $pwd\cf-credentials\cf-password -Raw).trim()
 $Env:CF_INT_OIDC_PASSWORD=(Get-Content $pwd\cf-credentials\uaa-oidc-password -Raw).trim()
 $Env:CF_INT_OIDC_USERNAME="admin-oidc"
-$Env:CF_INT_API=(Get-Content $pwd\cf-credentials\cf-api -Raw).trim()
+$Env:CF_INT_API="https://api.$CF_INT_NAME.cf-app.com"
 $Env:CF_DIAL_TIMEOUT=15
 $Env:SKIP_SSL_VALIDATION="false"
 
-$CF_INT_NAME = $DOMAIN.split(".")[0]
+# $CF_INT_NAME = $DOMAIN.split(".")[0]
 # Import-Certificate -Filepath "$pwd\cf-credentials\cert_dir\$CF_INT_NAME.lb.cert" -CertStoreLocation "cert:\LocalMachine\root"
 Import-Certificate -Filepath "$pwd\cf-credentials\cert_dir\$CF_INT_NAME.router.ca" -CertStoreLocation "cert:\LocalMachine\root"
 
