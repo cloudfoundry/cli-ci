4ef991f1f3b3c34b2f093945bff91498beae7cf3
gururajsh <gururajsh@vmware.com>
2022-08-15 13:14:02 -0400

    Fix syntax

--- ci/cli-v8-toolsmiths/tasks/integration-windows.ps1
+++ ci/cli-v8-toolsmiths/tasks/integration-windows.ps1
@@ -5,9 +5,9 @@ $ErrorActionPreference = "Stop"
 cd $Env:GOPATH\src\code.cloudfoundry.org\cli
 
 $CF_PASSWORD=${CF_INT_PASSWORD}
-.\cf api $CF_INT_API --skip-ssl-validation
-.\cf auth
-.\cf enable-feature-flag route_sharing
+.\cf.exe api $CF_INT_API --skip-ssl-validation
+.\cf.exe auth
+.\cf.exe enable-feature-flag route_sharing
 
 ginkgo.exe -r `
 	-nodes=16 `
